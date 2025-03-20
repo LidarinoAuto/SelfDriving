cf53b19643923714ea791cb2ba5c366e4b9059c0
Lidarino <lidarino.ekot@gmail.com>
2025-03-20 18:57:10 +0100

    lagt til komunikasjon mellom IR og Raspberry
    lagt til stop start lidar og stop start robot

--- ESP32/Fremdrift/IRControl.h
+++ ESP32/Fremdrift/IRControl.h
@@ -1,74 +1,117 @@
 #ifndef IR_CONTROL_H
 #define IR_CONTROL_H
-
+ 
 #include "PinConfig.h"
 #include <IRremote.h>
-
+ 
 // Extern variables shared with main code
 extern int speedX;
 extern int speedY;
 extern int rotation;
-
-// Timeout in ms
-//static const unsigned long IR_TIMEOUT = 500; // e.g. 500 ms
-
-// We store the last time we received any IR signal
+ 
+// Variabler for å styre robotens start/stopp
+bool robotStarted = false;
+bool lidarStarted = false;
+ 
+// Timeout i ms
 static unsigned long lastIRReceiveTime = 0;
-
+ 
 inline void IRSetup() {
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   lastIRReceiveTime = millis(); // Initialize at startup
+  Serial.begin(115200); // Seriell kommunikasjon til debugging
+  Serial2.begin(115200); // Seriell til Raspberry Pi (Lidar)
 }
-
+ 
 inline void handleIRInput() {
-  // Check if there's a new IR command
   if (IrReceiver.decode()) {
-    lastIRReceiveTime = millis(); // We got a new IR signal right now
-
+    lastIRReceiveTime = millis(); // Oppdater tidspunkt for siste signal
+ 
     Serial.print("IR cmd: ");
     Serial.println(IrReceiver.decodedIRData.command, HEX);
-
+ 
     switch (IrReceiver.decodedIRData.command) {
       case 0x18: // Forward
-        speedX = 0;    
-        speedY = 300;  
-        rotation = 0;
+        if (robotStarted) {
+          speedX = 0;    
+          speedY = 300;  
+          rotation = 0;
+        }
         break;
       case 0x52: // Backward
-        speedX = 0;
-        speedY = -300;
-        rotation = 0;
+        if (robotStarted) {
+          speedX = 0;
+          speedY = -300;
+          rotation = 0;
+        }
         break;
       case 0x8:  // Left
-        speedX = -300;
-        speedY = 0;
-        rotation = 0;
+        if (robotStarted) {
+          speedX = -300;
+          speedY = 0;
+          rotation = 0;
+        }
         break;
       case 0x5A: // Right
-        speedX = 300;
-        speedY = 0;
-        rotation = 0;
-        break;
-      case 0x1C: // Stop
-        speedX = 0;
-        speedY = 0;
-        rotation = 0;
+        if (robotStarted) {
+          speedX = 300;
+          speedY = 0;
+          rotation = 0;
+        }
         break;
       case 0x16: // Rotate left
-        speedX = 0;
-        speedY = 0;
-        rotation = -2;
+        if (robotStarted) {
+          speedX = 0;
+          speedY = 0;
+          rotation = -2;
+        }
         break;
       case 0xD: // Rotate right
+        if (robotStarted) {
+          speedX = 0;
+          speedY = 0;
+          rotation = 2;
+        }
+        break;
+      case 0x1C: // Start (med 2 sek delay)
+        Serial.println("Starting robot in 2 sec...");
+        delay(2000);
+        robotStarted = true;
+        Serial.println("Robot started!");
+        break;
+      case 0x19: // Stop (stopper også Lidar)
+        robotStarted = false;
         speedX = 0;
         speedY = 0;
-        rotation = 2;
+        rotation = 0;
+        Serial.println("Robot stopped!");
+ 
+        // Stopp også Lidar hvis den var på
+        if (lidarStarted) {
+          Serial.println("Stopping Lidar...");
+          Serial2.println("STOP_LIDAR");
+          lidarStarted = false;
+        }
+        break;
+      case 0x45: // Start Lidar
+        if (!lidarStarted) {
+          Serial.println("Starting Lidar...");
+          Serial2.println("START_LIDAR");
+          lidarStarted = true;
+        }
+        break;
+      case 0x46: // Stop Lidar
+        if (lidarStarted) {
+          Serial.println("Stopping Lidar...");
+          Serial2.println("STOP_LIDAR");
+          lidarStarted = false;
+        }
         break;
       default:
         Serial.println("Unknown command");
         break;
     }
-
+ 
     IrReceiver.resume();
   }
 
