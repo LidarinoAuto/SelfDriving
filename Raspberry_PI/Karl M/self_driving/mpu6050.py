55ec6feafd0479b537810061a2836412b424af32
karola-992 <e-karmyr1@iskule.no>
2025-04-26 16:35:05 +0200

    Update mpu6050.py

--- Raspberry_PI/Karl M/self_driving/mpu6050.py
+++ Raspberry_PI/Karl M/self_driving/mpu6050.py
@@ -10,6 +10,14 @@ def setup_mpu6050():
     bus.write_byte_data(MPU6050_ADDR, 0x6B, 0x00)
     time.sleep(0.1)
 
+def read_gyro_z_raw():
+    high = bus.read_byte_data(MPU6050_ADDR, 0x47)
+    low = bus.read_byte_data(MPU6050_ADDR, 0x48)
+    value = (high << 8) | low
+    if value >= 0x8000:
+        value = -((65535 - value) + 1)
+    return -(value / 131.0)
+
 def read_gyro_z():
     # Gyro Z er på register 0x47 og 0x48
     high = bus.read_byte_data(MPU6050_ADDR, 0x47)
